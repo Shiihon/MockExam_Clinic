2bb55721900f7b66d34102e68ccee9a29502218b
Shiihon <144337411+Shiihon@users.noreply.github.com>
2024-12-12 11:44:40 +0100

    added interface for AppointmentController

---
+++ src/main/java/app/controllers/ControllerAppointment.java
@@ -0,0 +1,11 @@
+package app.controllers;
+
+import io.javalin.http.Context;
+
+public interface ControllerAppointment {
+    public void getAll(Context ctx);
+    public void getById(Context ctx);
+    public void create(Context ctx);
+    public void update(Context ctx);
+    public void delete(Context ctx);
+}
