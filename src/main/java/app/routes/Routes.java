2cb0202e7b2c1c65873de17ad298c02439efa751
Shiihon <144337411+Shiihon@users.noreply.github.com>
2024-12-12 11:46:04 +0100

    implemented appointmentRoutes

--- src/main/java/app/routes/Routes.java
+++ src/main/java/app/routes/Routes.java
@@ -8,14 +8,17 @@ import static io.javalin.apibuilder.ApiBuilder.path;
 public class Routes {
 
     private static DoctorRoutes doctorRoutes;
+    private static AppointmentRoutes appointmentRoutes;
 
     public Routes(EntityManagerFactory emf) {
         doctorRoutes = new DoctorRoutes(emf);
+        appointmentRoutes = new AppointmentRoutes(emf);
     }
 
     public EndpointGroup getApiRoutes() {
         return () -> {
             path("/doctors", doctorRoutes.getDoctorRoutes());
+            path("/appointments", appointmentRoutes.getAppointmentRoutes());
         };
     }
 }
